bdacca6bd84f8c9767c2bce2bf887063ef07fcb2
Sharut Gupta <mt6170250@iitd.ac.in>
2022-06-07 16:28:38 +0530

    Update sketch.js

--- DarkRooms/sketch.js
+++ DarkRooms/sketch.js
@@ -1,11 +1,18 @@
-let nrows = 6;
-let ncols = 6;
-let maxwidth = (600 - 10 * (ncols+1))/ncols;
+let nrows = 9;
+let ncols = 8;
+let WIDTH;
+let HEIGHT;
+let maxwidth ;
+let maxheight ;
 let my_colors = ['#8E2D2C', '#BB6C28', '#E19E37', '#419094', '#A2D0BC', '#285D6A', '#E8D7A8' ]
 
 var num=0;
 function setup() {
-  createCanvas(600, 600);
+  WIDTH = windowWidth;
+  HEIGHT = windowHeight;
+  maxwidth = (WIDTH - 10 * (nrows+1))/nrows;
+  maxheight = (HEIGHT - 10 * (ncols+1))/ncols;
+  createCanvas(WIDTH, HEIGHT);
   background(0);
   fill(0);
   down = 0;
@@ -14,7 +21,7 @@ function setup() {
     for(let j=0;j<nrows;j++){
       num = int(random(0, 7));
       hole_num = int(random(0, 7));
-      my_rect(right+10,down+10,maxwidth,maxwidth, num, hole_num); 
+      my_rect(right+10,down+10,maxwidth,maxheight, num, hole_num); 
       right = right + maxwidth +10;
     }
     down = down + maxwidth +10;
