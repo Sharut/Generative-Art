97c9554dca51baada169244fb3129a23a2d811da
Sharut Gupta <mt6170250@iitd.ac.in>
2022-06-19 14:14:44 +0530

    Update sketch.js

--- Tesseract/sketch.js
+++ Tesseract/sketch.js
@@ -15,8 +15,8 @@ function setup() {
   h = windowHeight;
   r=50;
   la = min(w,h)/6;
-  lb = min(w,h)/3;
-  lc = min(w,h);
+  lb = min(w,h)/6;
+  lc = min(w,h)/9;
   background(0);
   fill(0);
   
@@ -30,7 +30,7 @@ function draw() {
   noStroke();
   ellipse(0, 0, 2*r);
   num = int(random(0, 7));
-  tesseract(num, PI/2)
+  tesseract(num, PI/3)
   
   
   
@@ -46,14 +46,23 @@ function tesseract(color_num, angle){
   y = r*sin(theta);
   x1 = (la+r)*cos(theta);
   y1 = (la+r)*sin(theta);
-  x2 = x1 + lb * cos(-angle+0.1+theta);
-  y2 = y1 + lb * sin(-angle+0.1+theta);
-  x3 = x2 + lc * cos(-2*angle+0.2+theta);
-  y3 = y2 + lc * sin(-2*angle+0.2+theta);
+  x2 = x1 + lb * cos(-angle+2*0.1+theta);
+  y2 = y1 + lb * sin(-angle+2*0.1+theta);
+  x3 = x2 + lb * cos(-2*angle+2*0.2+theta);
+  y3 = y2 + lb * sin(-2*angle+2*0.2+theta);
+  x4 = x3 + lb * cos(-3*angle+2*0.3+theta);
+  y4 = y3 + lb * sin(-3*angle+2*0.3+theta);
+  x5 = x4 + lb * cos(-4*angle+2*0.4+theta);
+  y5 = y4 + lb * sin(-4*angle+2*0.4+theta);
+  x6 = x5 + lb * cos(-5*angle+2*0.5+theta);
+  y6 = y5 + lb * sin(-5*angle+2*0.5+theta);
   
   line(x,y,x1,y1);
   line(x1,y1,x2,y2);
   line(x2,y2,x3,y3);
+  line(x3,y3,x4,y4);
+  line(x4,y4,x5,y5);
+  line(x5,y5,x6,y6);
   
 }
 
