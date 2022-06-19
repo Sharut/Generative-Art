2fa038132412fd87f31407067e6b9fa7b70b3ffb
Sharut Gupta <mt6170250@iitd.ac.in>
2022-06-19 14:15:13 +0530

    Update sketch.js

--- ShapeTiling/sketch.js
+++ ShapeTiling/sketch.js
@@ -27,9 +27,36 @@ function draw() {
     fill(my_colors[num]);
     rect(x, y, 50, 50);
     fill('white')
-    noStroke();
+    stroke('white');
+    strokeWeight(2);
     num = random(0.5, 1);
-    ellipse(x + 15*num, y + 15*num, 10,10);
+    start = 15;
+    p =  x + random(15, 30);
+    q = y + start;
+    // triangle(p, q, p, q+30, p+30, q+30);
+    // ellipse(x + 15*num, y + 15*num, 10,10);
+    ellipse(p,q, 11,11);
+    line(p,q, p,q+25);
+    // line(p,q+20, p-15,q+9);
+    // line(p,q+20, p+15,q+9);
+    
+    line(p,q+10, p-10,q+20);
+    line(p,q+10, p+10,q+20);
+    
+    line(p,q+25, p-10,q+50-start);
+    line(p,q+25, p+10,q+50-start);
+    
+    strokeWeight(0.5);
+    num = random(0.3, 0.7);
+    line(x, y + 50*num, x+50,y + 50*num);
+    gap1 = random(0.05, min(num, 1-num));
+    line(x, y + 50*(num+gap1), x+50,y + 50*(num+gap1));
+    
+    vert = random(0.3, 0.7);
+    line(x + 50*vert , y , x + 50*vert ,y + 50);
+    line(x + 50*(vert+gap1) , y , x + 50*(vert+gap1) ,y + 50);
+    
+    
   }
 
   x += 50;
